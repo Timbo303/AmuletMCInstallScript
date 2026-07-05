8def78b93979626b4351f5349108538a212d71d9
Timbo303 <tdshepherd512@gmail.com>
2026-07-05 15:29:09 -0500

    Update AmuletMCInstallScript.sh
    Attempting to add Fedora and Arch Detection

--- AmuletMCInstallScript.sh
+++ AmuletMCInstallScript.sh
@@ -5,23 +5,23 @@
 # Note: On macOS, use 'brew install cmake' and 'xcode-select --install' instead.
 echo "Installing build dependencies..."
 if command -v apt-get &> /dev/null; then
-	echo "Debian-based system detected."
-	sudo apt-get update && upgrade -y
-	curl -LsSf https://astral.sh/uv/install.sh | sh
-    uv python install 3.12
+	echo "Debian-based system detected"
 	sudo apt-get update
 	sudo apt-get install -y cmake build-essential git libgtk-3-dev pkg-config
-#elif command -v dnf &> /dev/null; then
-    #echo "Red Hat-based system detected. Building on Fedora is currently broken due to a bug I can't figure out how to fix."
-    #sudo dnf install -y @development-tools
-    #sudo dnf install -y cmake python3.12-devel git gtk3-devel pkgconfig gcc-c++ gawk
-#elif command -v pacman &> /dev/null; then
-    #echo "Arch-based system detected. Work in progress"
-    #sudo pacman -Syu --needed git cmake base-devel python3.12 pkgconf gtk3
+elif command -v dnf &> /dev/null; then
+    echo "Red Hat-based system detected"
+    sudo dnf install -y @development-tools
+    sudo dnf install -y cmake git gtk3-devel pkgconfig gcc-c++ gawk
+elif command -v pacman &> /dev/null; then
+    echo "Arch-based system detected"
+    sudo pacman -Syu --needed git cmake base-devel pkgconf gtk3
 else
     echo "Unsupported package manager. Work in progress for Arch. Fedora builds are bugged at rocksdb compiling currently. Exiting."
     exit 1
 fi
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv python install 3.12
 
 # 2. Create and activate a Python virtual environment
 echo "Creating Python virtual environment..."
@@ -42,4 +42,4 @@ export CXXFLAGS+=" -Wno-error=maybe-uninitialized"
 uv pip install --upgrade amulet-map-editor --verbose
 
 echo "Installation complete!"
-echo "You can launch the editor anytime by activating the environment and running: uv amulet_map_editor"
+echo "You can launch the editor anytime by activating the environment and running: uv run amulet_map_editor"
